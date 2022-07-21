26974afe42e172274e7906ba9f04ff2e83bf47df
michaleona <patricia_jelcika@yahoo.com>
2022-07-21 10:12:29 +0000

    Deploying to gh-pages from @ michaleona/kitalulus-test@a71e73ed94276787823bf91a75c9c517a4877090 🚀

---
+++ _next/static/zLZkq0OLS4Ncesv2bxZwb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/762-bde7c1ec0373972f.js","static/chunks/pages/index-83a40903a2c99c4f.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
